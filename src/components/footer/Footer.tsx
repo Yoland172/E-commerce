d00f77ad836a544380b1f2429675a0e73f57a0f0
Yoland172 <volodvolodimer@gmail.com>
2024-07-04 19:33:44 +0300

    refactoring footerSection

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,42 +2,59 @@ import React from "react";
 import styles from "./footer.module.scss";
 
 const Footer = () => {
+  const socilas = [
+    "Tiktok",
+    "Instagram",
+    "Facebook",
+    "X",
+    "Pinterest",
+    "Youtube",
+  ];
+
+  const helpContainer = [
+    "Shiping",
+    "Payment and invoices",
+    "Exchanges, returns and refunds",
+  ];
+
+  const companyContainer = ["About us", "Join life", "Stores"];
+
+  const policiesContainer = ["Privacy Policy", "Purchase condition"];
+
   return (
     <div className={styles.main}>
       <div className={styles.sectionLine}></div>
       <ul className={styles.socialsContainer}>
-        <li className={styles.social}>Tiktok</li>
-        <li className={styles.social}>Instagram</li>
-        <li className={styles.social}>Facebook</li>
-        <li className={styles.social}>X</li>
-        <li className={styles.social}>Pinterest</li>
-        <li className={styles.social}>Youtube</li>
+        {socilas.map((el) => {
+          return <li className={styles.social}>{el}</li>;
+        })}
       </ul>
 
       <div className={styles.infoContainer}>
         <ul className={styles.infoSection}>
           <li className={styles.title}> Help</li>
-          <li className={styles.info}>Shiping</li>
-          <li className={styles.info}>Payment and invoices</li>
-          <li className={styles.info}>Exchanges, returns and refunds</li>
+          {helpContainer.map((el) => {
+            return <li className={styles.info}>{el}</li>;
+          })}
         </ul>
         <ul className={styles.infoSection}>
           <li className={styles.title}>Company</li>
-          <li className={styles.info}>About us</li>
-          <li className={styles.info}>Join life</li>
-          <li className={styles.info}>Stores</li>
+          {companyContainer.map((el) => {
+            return <li className={styles.info}>{el}</li>;
+          })}
         </ul>
         <ul className={styles.infoSection}>
           <li className={styles.title}>Policies</li>
-          <li className={styles.info}>Privacy Policy</li>
-          <li className={styles.info}>Purchase condition</li>
+          {policiesContainer.map((el) => {
+            return <li className={styles.info}>{el}</li>;
+          })}
         </ul>
       </div>
       <div className={styles.privacyContainer}>
         <div className={styles.languageContainer}>
-        <p>Ukrainian</p>
-        <div></div>
-        <p>English</p>
+          <p>Ukrainian</p>
+          <div></div>
+          <p>English</p>
         </div>
         <h3>© All rights reserved</h3>
       </div>
