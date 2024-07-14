a7db17a2d10a81f828517313284e4dd4f61c74dc
Yoland172 <volodvolodimer@gmail.com>
2024-07-14 22:48:40 +0300

    refactoring page routing

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,76 +1,33 @@
 import React from "react";
-import { HashRouter as BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  HashRouter as BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import LoginContainer from "@components/login/LoginContainer";
-import Footer from "@components/footer/Footer";
-import HeaderContainer from "@components/header/HeaderContainer";
 import ProfilePageRouting from "@components/profile/ProfilePageRouting";
 import ProductContainer from "@components/productPage/ProductContainer";
 import MainPage from "@components/mainPage/MainPage";
 import CartContainer from "@components/cart/CartContainer";
 import "./App.module.scss";
 import CheckoutContainer from "@components/checkout/CheckoutContainer";
+import RoutingLayout from "@components/routingLayout/RoutingLayout";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route
-            path="profile/:tab"
-            element={
-              <>
-                <HeaderContainer />
-                <ProfilePageRouting />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            path="mainPage"
-            element={
-              <>
-                <HeaderContainer />
-                <MainPage />
-                <Footer />
-              </>
-            }
-          />
-
-          <Route
-            path="product/:id"
-            element={
-              <>
-                <HeaderContainer />
-                <ProductContainer />
-                <Footer />
-              </>
-            }
-          />
-
-          <Route
-            path="cart"
-            element={
-              <>
-                <HeaderContainer />
-                <CartContainer />
-                <Footer />
-              </>
-            }
-          />
-
-          <Route
-            path="cart/checkout"
-            element={
-              <>
-                <HeaderContainer />
-                <CheckoutContainer />
-                <Footer />
-              </>
-            }
-          />
           <Route path="/login" element={<LoginContainer />} />
-
-          {/* <Route path="*" element={<Navigate to={"/mainPage"} />} /> */}
+          <Route path="/" element={<RoutingLayout />}>
+            <Route path="mainPage" element={<MainPage />} />
+            <Route path="profile/:tab" element={<ProfilePageRouting />} />
+            <Route path="product/:id" element={<ProductContainer />} />
+            <Route path="cart" element={<CartContainer />} />
+            <Route path="cart/checkout" element={<CheckoutContainer />} />
+            <Route path="*" element={<Navigate to="/mainPage" replace />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
